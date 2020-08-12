5d6773d7ae441adea56f39a4fd96779542f57879
brynphillips <17993329+brynphillips@users.noreply.github.com>
2020-08-11 23:50:32 -0700

    changed the time counter and tests

--- pomodoro.py
+++ pomodoro.py
@@ -2,20 +2,35 @@ from datetime import datetime
 
 
 class _pomodoro:
-    def __init__(self, count=0, time=0):
+    def __init__(self, count, time):
         self.count = count
+        self.time = time
 
     def get_count(self) -> int:
         return self.count
 
-    def start_timer(self) -> None:
+    def start_timer(self) -> datetime:
         self.time = datetime.now(tz=None)
+        return self.time
+
+
+def pomodoro_start() -> bool:
+    pomodoro = _pomodoro(0, datetime.now())
+    pomodoro.count += 1
+    pomodoro.start_timer
+    print('Started!')
+    print(pomodoro.time)
+    while True:
+        timedelta = datetime.now(tz=None) - pomodoro.time
+        if timedelta.total_seconds() % 60 == 0:
+            print(f'{int(timedelta.total_seconds()//60)} minutes has elapsed.')
+        if timedelta.total_seconds() == 1500:
+            print('Time over!')
+            return False
 
 
 def main():
-    pomodoro = _pomodoro()
-    pomodoro.count += 1
-    print(pomodoro.get_count())
+    pomodoro_start()
 
 
 if __name__ == '__main__':
--- tests/pomodoro_test.py
+++ tests/pomodoro_test.py
@@ -1,9 +1,6 @@
-from datetime import datetime
-
 import pomodoro
 
 
 def test_initial_timer_start():
-    pomo = pomodoro._pomodoro()
-    pomo.start_timer()
-    assert isinstance(pomo.time, datetime)
+    test = pomodoro.pomodoro_start()
+    assert test is False
